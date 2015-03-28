ebaa7b18d93700a5a2aa30f211f9bee459d0a6fd
Sean <sean>
2015-03-28 16:28:40 +0000

    basic nav

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+ * @todo a proper submission handler, ie; are there errors? stay on the current page
+ */
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +19,37 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AppBundle:default:index.html.twig');
+    }
+    
+    /**
+     * @Route("/page_history/", name="page-history")current_page
+     */
+    public function pageHistoryAction(Request $request)
+    {
+        // we'll assume if they're on page X they've completed all pages up to X
+        $current_page = 1;
+        $direction = 'forward';
+        if(is_numeric($request->query->get('current_page'))) {
+            $current_page = (int)$request->query->get('current_page');
+        }
+        if(in_array($request->query->get('direction'),array('forward','back'))) {
+            $direction = $request->query->get('direction');
+        }
+        
+        // build the history
+        $history = range(1,$current_page);
+        $current_page += $direction=='back'? -1 : 1;
+        $current_page = max(1, $current_page);
+        
+        // prepare response
+        $data = array(
+            'page_history' => $history,
+            'current_page' => $current_page
+        );
+        
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
---
+++ Resources/public/js/main.js
@@ -0,0 +1,35 @@
+// what page are we currently on? which have we been on before?
+var current_page = 1;
+var page_history = [];
+
+$(document).ready(function(){
+
+    // setup the form submission ajax handling
+    $('.go').click(function(e,ui){
+        e.preventDefault();
+        
+        // get direction
+        var direction = $(this).hasClass('go-back')? 'back' : 'forward';
+        
+        // do ajax call. we get an array in return telling us what pages we've been on before
+        $.ajax({
+            'url': '/page_history',
+            'data': {
+                'direction': direction,
+                'current_page': current_page
+                },
+            'success': handleResponse
+        });
+    });
+});
+
+/**
+ * @brief take a page history array, store data and update content
+ */
+function handleResponse(data)
+{
+    page_history = data['page_history'];
+    current_page = data['current_page'];
+    
+    $('#ajax-section .content').html(current_page);
+}
